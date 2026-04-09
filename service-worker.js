11e2ec6083ede74080d19f8c5ed056ca6bad2b1d
janezjanijanes-sketch <janez.jani.janes@gmail.com>
2026-04-09 19:54:53 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+{
+  "name": "Zaloga App",
+  "short_name": "Zaloga",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#4CAF50",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
